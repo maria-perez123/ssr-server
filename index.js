d894e130e2d322b5903033c7580de029b9001ab8
María Pérez <maria_erez@hotmail.es>
2020-09-23 18:51:33 -0500

    implementación user movies

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ app.post("/auth/sign-in", async function(req, res, next) {
   })(req, res, next);
 });
 
-app.post("/auth/sign-up", async function(req, res, next) {
-  const{body:user}=req;
+app.post('/auth/sign-up', async function(req, res, next) {
+  const {body:user} =req;
   try{
     await axios({
-      url:`${config.apiUrl}(api/auth/sign-up)`,
+      url:`${config.apiUrl}/api/auth/sign-up`,
       method:'post',
       data:'user'
     })
@@ -58,11 +58,48 @@ app.get("/movies", async function(req, res, next) {
 });
 
 app.post("/user-movies", async function(req, res, next) {
+  try {
+    const { body: userMovie } = req;
+    const { token } = req.cookies;
+    console.log(token);
+    console.log(userMovie);
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: 'post',
+      data: userMovie
+    });
+    console.log(data);
+    console.log(status);
+    if (status !== 201) {
+      return next(boom.badImplementation());
+    }
 
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.delete("/user-movies/:userMovieId", async function(req, res, next) {
+  try {
+    const { userMovieId } = req.params;
+    const { token } = req.cookies;
 
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "delete"
+    });
+
+    if (status !== 200) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.listen(config.port, function() {
